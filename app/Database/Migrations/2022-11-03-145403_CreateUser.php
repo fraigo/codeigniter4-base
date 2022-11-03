801e2b25c51891aefc2724b64ac2fe01865bdd64
Francisco <franciscoigor@gmail.com>
2022-11-03 07:54:37 -0700

    php spark make:migration CreateUser

---
+++ app/Database/Migrations/2022-11-03-145403_CreateUser.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateUser extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '64',
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => '128',
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => '32',
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('user');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('user');
+    }
+}
