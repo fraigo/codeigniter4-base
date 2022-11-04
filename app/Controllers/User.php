5dde71b14d59bb3ce466b60b7102f48f1fe22463
Francisco <franciscoigor@gmail.com>
2022-11-03 20:29:02 -0700

    Setup RESTful controller for User

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -2,12 +2,16 @@
 
 namespace App\Controllers;
 
-use App\Controllers\BaseController;
+use CodeIgniter\RESTful\ResourceController;
 
-class User extends BaseController
+class User extends ResourceController
 {
+    protected $modelName = 'App\Models\User';
+    protected $format    = 'json';
+
     public function index()
     {
-        //
+        return $this->respond($this->model->findAll());
     }
+
 }
