dc8986aad288952752af3753652a43b4c33e0055
Francisco <franciscoigor@gmail.com>
2022-11-10 08:42:57 -0800

    Generalize and create base resource controller

---
+++ app/Controllers/BaseResourceController.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\RESTful\ResourceController;
+
+class BaseResourceController extends ResourceController
+{
+    protected function result($data, $success=true){
+        return $this->respond([
+            "success" => $success,
+            "data" => $data
+        ]);
+    }
+
+    protected function is_admin(){
+        $user = session('user');
+        return $user['is_admin'];
+    }
+
+    protected function error($errors=[],$code=400){
+        $messages = [];
+        if (!is_array($errors)){
+            $errors = [$errors];
+        }
+        foreach($errors as $fld=>$err){
+            $messages[] = $err;
+        }
+        return $this->response->setStatusCode($code)->setJSON([
+            "success" => false,
+            "data" => null,
+            "errors" => $errors,
+            "message" => implode('\n',$messages)
+        ]);
+    }
+
+    protected function validateWithRules($data,$rules){
+        $validation = \Config\Services::validation();
+        $validation->setRules($rules);
+        if (!$validation->run($data)){
+            return $validation->getErrors();
+        }
+        return null;
+    }
+
+    protected function getById($id){
+        return $this->filteredModel()->find($id);
+    }
+
+    protected function selectFields(){
+        return [];
+    }
+
+    protected function filterModel($model){
+        return $model;
+    }
+
+    protected function filteredModel(){
+        return $this->filterModel($this->model->select($this->selectFields()));
+    }
+
+    protected function restricted($action){
+        return in_array($action,$this->restrictedActions());
+    }
+
+    protected function restrictedActions(){
+        return [];
+    }
+
+    protected function actionRules($action){
+        return [];
+    }
+
+    protected function formatRequest($request){
+        return $request;
+    }
+
+    protected function addAllowedFields($action){
+        
+    }
+
+    public function index()
+    {
+        return $this->result($this->filteredModel()->findAll());
+    }
+
+    public function show($id = null){
+        $item = $this->getById($id);
+        if (!$item){
+            return $this->error(["Not found"]);
+        }
+        return $this->result($item);
+    }
+
+    public function create($id = null){
+        if ($this->restricted("create")){
+            return $this->error(["Not allowed to create"],401);
+        }
+        $this->addAllowedFields("create");
+        $request = $this->request->getJSON(true);
+        $rules = $this->actionRules("create");
+        $errors = $this->validateWithRules($request,$rules);
+        if ($errors){
+            return $this->error($errors);
+        }
+        $request = $this->formatRequest($request);
+        $result = $this->model->insert($request);
+        $last_id = $this->model->getInsertID();
+        return $this->result(
+            $this->getById($last_id),
+            $result?true:false
+        );
+    }
+
+    public function update($id = null){
+        if ($this->restricted("update")){
+            return $this->error(["Not allowed to update"],401);
+        }
+        $item = $this->getById($id);
+        if (!$item){
+            return $this->error(["Not found"]);
+        }
+        $request = $this->request->getJSON(true);
+        $this->addAllowedFields("update");
+        $rules = $this->actionRules("update");
+        $errors = $this->validateWithRules($request,$rules);
+        if ($errors){
+            return $this->error($errors);
+        }
+        $request = $this->formatRequest($request);
+        $result = $this->model->update($id,$request);
+        return $this->result(
+            $this->getById($id),
+            $result?true:false);
+    }
+
+    public function delete($id = null){
+        if ($this->restricted("delete")){
+            return $this->error(["Not allowed to delete"],401);
+        }
+        $item = $this->getById($id);
+        if (!$item){
+            return $this->error(["Not found"]);
+        }
+        $result = $this->model->delete($id);
+        return $this->result([
+            "id" => $id
+            ], $result?true:false);
+    }
+
+
+}
--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -2,138 +2,63 @@
 
 namespace App\Controllers;
 
-use CodeIgniter\RESTful\ResourceController;
-
-class User extends ResourceController
+class User extends BaseResourceController
 {
     protected $modelName = 'App\Models\User';
     protected $format    = 'json';
 
-    public function result($data, $success=true){
-        return $this->respond([
-            "success" => $success,
-            "data" => $data
-        ]);
+    protected function selectFields(){
+        return ["id","name","email","is_admin"];
     }
 
-    public function error($errors=[],$code=400){
-        $messages = [];
-        if (!is_array($errors)){
-            $errors = [$errors];
+    protected function filterModel($model){
+        if (!$this->is_admin()){
+            $model->where('is_admin',0);
         }
-        foreach($errors as $fld=>$err){
-            $messages[] = $err;
+        return $model;
+    }
+
+    protected function restrictedActions(){
+        if (!$this->is_admin()){
+            return ["create","delete"];
         }
-        return $this->response->setStatusCode($code)->setJSON([
-            "success" => false,
-            "data" => null,
-            "errors" => $errors,
-            "message" => implode('\n',$messages)
-        ]);
+        return [];
     }
 
-    private function getById($id){
-        $user = session('user');
-        if ($user['is_admin']){
-            return $this->model->select(["id","name","email","is_admin"])->find($id);
-        } else {
-            return $this->model->select(["id","name","email","is_admin"])->where('is_admin',0)->find($id);
-        }
-    }
-
-    public function index()
-    {
-        $user = session('user');
-        if ($user['is_admin']){
-            return $this->result($this->model->select(["id","name","email","is_admin"])->findAll());
-        } else {
-            return $this->result($this->model->select(["id","name","email","is_admin"])->where('is_admin',0)->findAll());
-        }
-    }
-
-    public function show($id = null){
-        return $this->result($this->getById($id));
-    }
-
-    public function create($id = null){
-        $user = session('user');
-        if (!$user['is_admin']){
-            return $this->error(["Not allowed to create"],401);
-        }else{
+    protected function addAllowedFields($action){
+        if ($this->is_admin()){
             $this->model->addAllowedFields(["is_admin"]);
         }
-        $request = $this->request->getJSON(true);
-        $rules = [
-            'name' => 'required',
-            'password' => 'required',
-            'email'    => 'required|valid_email',
-        ];
-        $validation = \Config\Services::validation();
-        $validation->setRules($rules);
-        if (!$validation->run($request)){
-            $errors = $validation->getErrors();
-            return $this->error($errors);
-        }
-        $request["password"] = md5($request["password"]);
-        $result = $this->model->insert($request);
-        $last_id = $this->model->getInsertID();
-        return $this->result(
-            $this->getById($last_id),
-            $result?true:false);
     }
 
-    public function update($id = null){
-        $item = $this->getById($id);
-        if (!$item){
-            return $this->error(["Not found"]);
-        }
-        $request = $this->request->getJSON(true);
-        $user = session('user');
-        if (!$user['is_admin']){
-            if ($item["email"]!=$user["email"]){
-                return $this->error(["Cannot modify other users"]);
-            }
-            if ($request["email"] != $item["email"]){
-                return $this->error(["Cannot modify email"]);
-            }
-        }else{
-            $this->model->addAllowedFields(["is_admin"]);
-        }
+    protected function actionRules($action){
         $rules = [
             'name' => 'required',
             'email'    => 'required|valid_email',
+            'password' => 'required|min_length[6]',
+            'is_admin' => 'if_exist|in_list[0,1]',
         ];
-        $validation = \Config\Services::validation();
-        $validation->setRules($rules);
-        if (!$validation->run($request)){
-            $errors = $validation->getErrors();
-            return $this->error($errors);
+        if ($action=="update"){
+            $rules["password"]='if_exist|min_length[6]';
         }
+        return $rules;
+    }
+
+    protected function formatRequest($request){
         if (!empty(@$request["password"])){
             $request["password"] = md5($request["password"]);
         }
-        $result = $this->model->update($id,$request);
-        return $this->result(
-            $this->getById($id),
-            $result?true:false);
+        return $request;
     }
 
-    public function delete($id = null){
-        $user = session('user');
-        if (!$user['is_admin']){
-            return $this->error("Not allowed to delete",401);
-        }
+
+    public function delete($id=null){
         $item = $this->getById($id);
-        if (!$item){
-            return $this->error(["Not found"]);
-        }
+        $user = session("user");
         if ($item["email"]==$user["email"]){
             return $this->error(["Cannot delete yourself"]);
         }
-        $result = $this->model->delete($id);
-        return $this->result([
-            "id" => $id
-            ], $result?true:false);
+        return parent::delete($id);
     }
 
 }
